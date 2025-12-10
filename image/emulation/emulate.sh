3af9e694ba254ce0fe31682bfcfdc0ef758dd2e3
Engin Cilasun <ecilasun@me.com>
2025-12-09 16:12:57 -0800

    new quemu binary and emulator script

--- image/emulation/emulate.sh
+++ image/emulation/emulate.sh
@@ -1,1 +1,2 @@
-../../qemu/build/qemu-system-arm -m 1024 -M xilinx-zynq-a9 -kernel ./zImage -gdb tcp::9000 -dtb ./system.dtb -initrd ./rootfs.cpio.gz.u-boot -serial stdio
+../../qemu/build/qemu-system-arm -m 1024 -M xilinx-zynq-a9 -kernel ./zImage -gdb tcp::9000 -dtb ./system.dtb -initrd ./rootfs.cpio.gz.u-boot -display sdl,grab-mod=lctrl-lalt -serial mon:stdio -usb -device usb-kbd
+
